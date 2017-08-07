14bb7ccaba9769ad7a372064ef6d1e7e3c7ba62e
Richard Young <richard.young@utexas.edu>
2017-08-06 22:49:56 -0500

    log port

--- app/index.js
+++ app/index.js
@@ -5,5 +5,5 @@ const app = express();
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html');
 }).listen(port, () => {
-    console.log('Listening on port 3000...');
+    console.log('Listening on port: ' + port);
 });
