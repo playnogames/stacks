456cbd3c53bd70ccac21e1e4415cf83e6eec589d
Benita <benitazhang@gmail.com>
2017-08-13 14:54:24 -0500

    routing based on login status

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import utils from '../utils';
 
+import utils from '../utils';
 import Login from './Login';
 import Dashboard from './Dashboard';
 
+
+function loggedIn() {
+	return false;
+}
+
+function requireAuth(component) {
+	return (
+		loggedIn() ? component : <Login />
+	);
+}
 
 class App extends React.Component {
 	constructor(){
@@ -12,16 +22,13 @@ class App extends React.Component {
 	}
 
 	render(){
-		return(
+		return (
 			<BrowserRouter>
 				<Switch>
-					{ !utils.isLoggedIn() &&
-						<Route path='/' component={ Login }/>
-					}
-					<Route exact path='/' component={ Dashboard } />
+					<Route exact path='/' render={ () => (requireAuth(<Dashboard/>)) } />
 				</Switch>
 			</BrowserRouter>
-		)
+		);
 	}
 }
 
